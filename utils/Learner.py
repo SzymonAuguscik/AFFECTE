e5b3bd8ae35bca3cba7893593fe277b5a91f1091
SzymonAuguscik <szyauguscik@gmail.com>
2024-10-10 19:43:56 +0200

    Move LinkContraintsLoss to separate file

--- utils/Learner.py
+++ utils/Learner.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from utils.LinkConstraintsLoss import LinkConstraintsLoss
 from sklearn.exceptions import UndefinedMetricWarning
 from constants import Results, Paths, Hyperparameters
 from typing import Dict, List, TextIO, Optional
@@ -14,73 +15,6 @@ import torch
 import os
 warnings.filterwarnings("ignore", category=UndefinedMetricWarning)
 
-
-#TODO move to separate file
-class LinkConstraintsLoss(torch.nn.Module):
-    """
-    LinkConstraintsLoss is used as a regularization method. It relies on creating links between vectors/embeddings
-    from the same class.
-
-    Attributes
-    ----------
-    _device : torch.device
-        The device to perform calculation on.
-
-    Examples
-    --------
-    model_loss = torch.nn.BCELoss()
-    lc_loss = LinkConstraintsLoss("gpu")
-    model = ConvolutionalNeuralNetwork() # exemplary torch.nn.Module
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-    loss = 0
-    for X, y in zip(features, labels):
-        pred = model(X)
-        loss += model_loss(pred, y)
-        loss += lc_loss(X, y)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    See also
-    --------
-    https://bmcmedinformdecismak.biomedcentral.com/articles/10.1186/s12911-021-01546-2
-
-    """
-    def __init__(self, device: torch.device) -> None:
-        """
-        Initiate LinkConstraintsLoss with calculation device.
-
-        Parameters
-        ----------
-        device : torch.device
-            The device to perform calculation on.
-
-        """
-        super(LinkConstraintsLoss, self).__init__()
-        self._device: torch.device = device
-
-    def forward(self, beta: torch.Tensor, e: np.ndarray) -> float:
-        """
-        Perform forward pass of the data.
-
-        Parameters
-        ----------
-        beta : torch.Tensor
-            The input data to be propagated.
-        e : np.ndarray
-            Link coefficients to be used for link constraints.
-
-        Returns
-        -------
-        loss : float
-            The link constraints loss.
-
-        """
-        i: torch.Tensor = torch.arange(beta.size(0)).reshape(-1, 1).to(self._device)
-        j: torch.Tensor = torch.arange(beta.size(0)).to(self._device)
-        loss: float = 0.5 * torch.norm(beta[i, 0] - e[i, j] * beta[j, 0], p=2)
-        return loss
 
 class Learner:
     """
---
+++ utils/LinkConstraintsLoss.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+import torch
+
+
+class LinkConstraintsLoss(torch.nn.Module):
+    """
+    LinkConstraintsLoss is used as a regularization method. It relies on creating links between vectors/embeddings
+    from the same class.
+
+    Attributes
+    ----------
+    _device : torch.device
+        The device to perform calculation on.
+
+    Examples
+    --------
+    model_loss = torch.nn.BCELoss()
+    lc_loss = LinkConstraintsLoss("gpu")
+    model = ConvolutionalNeuralNetwork() # exemplary torch.nn.Module
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    loss = 0
+    for X, y in zip(features, labels):
+        pred = model(X)
+        loss += model_loss(pred, y)
+        loss += lc_loss(X, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    See also
+    --------
+    https://bmcmedinformdecismak.biomedcentral.com/articles/10.1186/s12911-021-01546-2
+
+    """
+    def __init__(self, device: torch.device) -> None:
+        """
+        Initiate LinkConstraintsLoss with calculation device.
+
+        Parameters
+        ----------
+        device : torch.device
+            The device to perform calculation on.
+
+        """
+        super(LinkConstraintsLoss, self).__init__()
+        self._device: torch.device = device
+
+    def forward(self, beta: torch.Tensor, e: np.ndarray) -> float:
+        """
+        Perform forward pass of the data.
+
+        Parameters
+        ----------
+        beta : torch.Tensor
+            The input data to be propagated.
+        e : np.ndarray
+            Link coefficients to be used for link constraints.
+
+        Returns
+        -------
+        loss : float
+            The link constraints loss.
+
+        """
+        i: torch.Tensor = torch.arange(beta.size(0)).reshape(-1, 1).to(self._device)
+        j: torch.Tensor = torch.arange(beta.size(0)).to(self._device)
+        loss: float = 0.5 * torch.norm(beta[i, 0] - e[i, j] * beta[j, 0], p=2)
+        return loss
